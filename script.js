dfc3e8a064d162af271993e6046535cf38ead981
Quadrizz <mackquadrizz@gmail.com>
2022-04-09 19:03:35 +0100

    recent changes

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const accordion = document.getElementsByClassName('question');
+    for (i=0; i<accordion.length; i++) {
+        accordion[i].addEventListener('click', function () {
+          this.classList.toggle('active')
+        })
+    }
+})
