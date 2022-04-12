1c0d44fea943e04901c04a39ec128c5a1a49b35a
Quadrizz <mackquadrizz@gmail.com>
2022-04-12 17:25:23 +0100

    close

--- script.js
+++ script.js
@@ -5,4 +5,28 @@ document.addEventListener('DOMContentLoaded', ()=>{
           this.classList.toggle('active')
         })
     }
+
+    document.getElementsByClassName('menu')[0].style.height = window.innerHeight + "px";
+
+    var menu = document.getElementsByClassName("menu_icon");
+    for(i=0; i<menu.length; i++){
+      if(i === 1){
+        menu[i].addEventListener('click', function(){
+          document.getElementsByClassName('menu')[0].classList.toggle('menu_show');
+          document.getElementsByTagName('body')[0].classList.toggle('body_fix')
+          setTimeout(()=>{
+            document.getElementsByClassName('menu')[0].style.display = 'none';
+          },500)
+        })
+      }
+      else{
+        menu[i].addEventListener('click', function(){
+          document.getElementsByClassName('menu')[0].style.display = 'flex';
+          setTimeout(()=>{
+            document.getElementsByClassName('menu')[0].classList.toggle('menu_show');
+            document.getElementsByTagName('body')[0].classList.toggle('body_fix')
+          },50)
+        })
+      }
+    }
 })
